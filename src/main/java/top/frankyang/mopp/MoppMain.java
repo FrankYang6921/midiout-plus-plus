8cf39f884b9045b00e6c3f86a92a6a71d31d34ae
Frank <yicheng_young@163.com>
2020-10-18 17:11:47 +0800

    Refactor stage.

--- src/main/java/top/frankyang/mopp/MoppMain.java
+++ src/main/java/top/frankyang/mopp/MoppMain.java
@@ -14,6 +14,7 @@ import java.util.Base64;
 import java.util.Objects;
 
 import static com.mojang.brigadier.arguments.StringArgumentType.*;
+import net.minecraft.client.particle.ParticleManager;
 
 public class MoppMain implements ModInitializer {
     private static final int MAJOR_VERSION = 0;
